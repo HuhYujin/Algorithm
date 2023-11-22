be01b2db5c3da4fed0480d0be24908ebb7c0cb3c
HuhYujin <yujin4802@gmail.com>
2023-11-22 14:47:57 +0900

    solved

--- src/SWEA/D1/SWEA_2072.java
+++ src/SWEA/D1/SWEA_2072.java
@@ -1,10 +1,27 @@
 package SWEA.D1;
 
+import java.util.Scanner;
+
 public class SWEA_2072 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//COMMIT
+	       Scanner sc = new Scanner(System.in);
+	        int T = sc.nextInt();
+
+	        for (int test_case = 1; test_case <= T; test_case++) {
+	            int sum = 0;
+
+	            for (int i = 0; i < 10; i++) {
+	                int num = sc.nextInt();
+	                if (num % 2 == 1) {  // 홀수인 경우에만 더하기
+	                    sum += num;
+	                }
+	            }
+
+	            System.out.println("#" + test_case + " " + sum);
+	        }
+
+	        sc.close();
 	}
 
 }
