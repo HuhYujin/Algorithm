ba9d93451f7dc8ffde4dab01208a06c7caf17d32
HuhYujin <yujin4802@gmail.com>
2023-12-21 11:03:49 +0900

    commit test

---
+++ src/SWEA/D3/SWEA_1225.java
@@ -0,0 +1,25 @@
+package SWEA.D3;
+
+import java.util.Scanner;
+
+public class SWEA_1225 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+	
+
+		}//end test_case
+		
+		sc.close();
+	}//end main
+
+}
